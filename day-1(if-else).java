901ab248294ad58dd26949c757865bc754e98c6c
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-21 20:45:28 +0530

    Day-1

--- day-1(if-else).java
+++ day-1(if-else).java
@@ -22,6 +22,33 @@ class Ifelse{
         }else{
             System.out.println("Number is not divisible by 5 and 11");
         }
-        
+        // Write a JAVA program to check whether a character is uppercase or lowercase
+        // alphabet.
+        System.out.println("Enter the Character: ");
+        char d = sc.next().charAt(0);
+        if(d>='A' && d<='Z'){
+            System.out.println("UpperCase");
+        }else if(d>='a' && d<='z'){
+            System.out.println("LowerCase");
+        }else{
+            System.out.println("Enter Again");
+        }
+        // Write a JAVA program to check whether the triangle is equilateral, isosceles or
+        // scalene triangle.
+        System.out.println("Enter the Side 1: ");
+        int e = sc.nextInt();
+        System.out.println("Enter the Side 2: ");
+        int f = sc.nextInt();
+        System.out.println("Enter the Side 3: ");
+        int g = sc.nextInt();
+
+        if(e==f && f==g){
+            System.out.println("Triangle is equilateral");
+        }else if(e==f || f==g){
+            System.out.println("Triangle is isosceles");
+        }else{
+            System.out.println("Triangle is scalene");
+        }
+
     }
 }
