a1b9491f10e4743c9ce568d5e5cc03f9ee3531f8
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-21 21:49:07 +0530

    Day-1

---
+++ day-1(for loop).java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Loop {
+    public static void main(String[] args) {
+        // Write a JAVA program to print all natural numbers from 1 to n
+        System.out.println("Enter the Number: ");
+        Scanner sc  = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i =1;i<=num;i++){
+            System.out.println(i);
+
+        }
+        // Write a JAVA C program to print all odd numbers from 1 to n
+        System.out.println("Enter the Number: ");
+        int a = sc.nextInt();
+        for(int j=1;j<=a;j++){
+            if(j%2!=0){
+                System.out.println(j);
+            }
+        }
+    }
+}
