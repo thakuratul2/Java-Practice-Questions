a9a72d7b6b2b399186f2b755e6214ab1bf0b8ba6
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-21 20:19:42 +0530

    Day-1

--- day-1(if-else).java
+++ day-1(if-else).java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 class Ifelse{
     public static void main(String[] args){
-        
+        // Write a JAVA program to find maximum between two numbers.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number: ");
+        int b = sc.nextInt();
+        if(a<b){
+            System.out.println(a+" is less than "+b);
+        }else{
+            System.out.println(a+" is greater than "+b);
+        }
     }
 }
