162c88cb921a9b37b96503a84c4481a31bf857da
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-22 23:20:29 +0530

    Day-2

---
+++ day-2(design-1).java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Design1{
+      public static void main(String[] args){
+        //Design-1 Solid Rectangle
+        Scanner sc = new Scanner(System.in);
+        int row,col;
+        System.out.println("Enter the number of Rows: ");
+        row = sc.nextInt();
+        System.out.println("Enter the number of Columns: ");
+        col = sc.nextInt();
+        //inner loop 
+        for(int i=1;i<=row;i++){
+            //outer loop
+            for(int j=1;j<=col;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+      }
+}
