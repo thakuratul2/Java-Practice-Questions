9b560166fa9b35d5e309cf4051ca3537eafd189a
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-23 00:17:41 +0530

    Day-2

---
+++ day-3(design-3).java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Design3 {
+    public static void main(String[] args) {
+        //Design 3 Half Pyramid
+        Scanner sc = new Scanner(System.in);
+        int i,j,row,col;
+        System.out.println("Enter the number of rows");
+        row = sc.nextInt();
+        System.out.println("Enter the number of columns");
+        col = sc.nextInt();
+        //outer loop
+        for(i=1;i<=row;i++)
+        {
+            //inner loop
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
