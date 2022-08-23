4165eade301f2639dc3f6a46bf813192ddcdac2f
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-23 22:04:14 +0530

    Day-3

---
+++ day-3(design-8).java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class design8 {
+    public static void main(String[] args) {
+        //reverse hill pyramid
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the number of columns: ");
+        int m = sc.nextInt();
+
+        //outer loop
+        for(int i=0;i<=n;i++){
+            //outer loop
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=n-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
