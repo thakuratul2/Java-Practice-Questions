7dabde8fa1908eea0b546a4b924c98b8d52aa003
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-23 13:09:39 +0530

    Day-2

--- day-2(design-6).java
+++ day-2(design-6).java
@@ -1,3 +1,21 @@
-class Design {
-    
+import java.util.Scanner;
+
+class Design6 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //half pyramid with numbers
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the number of columns: ");
+        int m = sc.nextInt();
+
+        //outer loop for rows
+        for(int i=1;i<=n;i++){
+            //inner loop for columns
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
