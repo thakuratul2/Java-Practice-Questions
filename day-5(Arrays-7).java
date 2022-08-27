1955486d9f0d931935a897f295ace82477772dd0
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-28 00:35:52 +0530

    Day-5

---
+++ day-5(Arrays-7).java
@@ -0,0 +1,12 @@
+class Arrays7 {
+    public static void main(String[] args) {
+        // Program to print the elements of an array present on odd position
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for(int i =0;i<arr.length;i++){
+            if(i%2!=0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
