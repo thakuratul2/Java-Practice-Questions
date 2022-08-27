24587b76018813a9824c35a76489f5cf3f646d48
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-28 00:36:53 +0530

    Day-5

--- day-5(Arrays-7).java
+++ day-5(Arrays-7).java
@@ -4,7 +4,7 @@ class Arrays7 {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         for(int i =0;i<arr.length;i++){
-            if(i%2!=0){
+            if(i%2==0){
                 System.out.print(arr[i]+" ");
             }
         }
