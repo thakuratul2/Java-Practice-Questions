3da456681a36e80e2b873f03f545d0e18cf86bf9
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-01 23:51:14 +0530

    Day-6

---
+++ Strings/day-6(Strings-5).java
@@ -0,0 +1,13 @@
+package Strings;
+
+class Strings5 {
+    public static void main(String[] args) {
+        //reverse a string 
+        String str = "Hello World";
+        String rev = "";
+        for(int i = str.length()-1;i>=0;i--){
+            rev = rev+str.charAt(i);
+            System.out.print(rev);
+        }
+    }
+}
