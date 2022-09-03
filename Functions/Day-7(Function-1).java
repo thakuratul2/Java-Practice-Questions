c27821a01cd9944740571b427b3be0ab6a5c6687
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-03 20:57:53 +0530

    Day-7

--- Functions/Day-7(Function-1).java
+++ Functions/Day-7(Function-1).java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 class Function1{
     public static void main(String[] args) {
-        
+        //swapping a variable by using third variable
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a: ");
+        int first = sc.nextInt();
+        System.out.println("Enter the value of b: ");
+        int second = sc.nextInt();
+        sc.close();
+        swap(first, second);
+       
+    }
+    public static void swap(int a, int b){
+        System.out.println("Before swapping: ");
+        System.out.println("Value of a = "+a);
+        System.out.println("Value of b = "+b);
+        int c = a;
+        a = b;
+        b= c;
+        System.out.println("After swapping: ");
+        System.out.println("Value of a = "+a);
+        System.out.println("Value of b = "+b);
     }
 }
