05454bd823484fedbd19ccdf24cd61a7de165dee
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-11 21:29:57 +0530

    Day-4

--- Basics/dy-4(pallidrome).java
+++ Basics/dy-4(pallidrome).java
@@ -7,7 +7,7 @@ class Pallidrome{
         int num = sc.nextInt();
         int temp = num;
         int rev = 0;
-        while (temp != 0) {
+        while (num> 0) {
             int rem = temp % 10;
             rev = rev * 10 + rem;
             temp = temp / 10;
