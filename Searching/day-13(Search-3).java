1304df07c00c49ac5261f547e5a45750e767225e
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-18 21:52:47 +0530

    Day-13

---
+++ Searching/day-13(Search-3).java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Search3{
+
+    public static void main(String[] args) {
+        //First and last occurrences of x
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+        System.out.println("Enter the element in arrays: ");
+        int[] arr = new int[size];
+
+        //loop
+        for(int i =0;i<size;i++){
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("Enter the element to find: ");
+        int key = sc.nextInt();
+        LinearAtul(arr, size, key);
+        sc.close();
+    }
+    public static int LinearAtul(int arr[],int n,int x){
+        int first = -1;
+        int last = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                if(first==-1){
+                    first = i;
+                }
+                last = i;
+            }
+        }
+        if(first!=-1){
+            System.out.println("First Occurrence: "+first);
+            System.out.println("Last Occurrence: "+last);
+        }else{
+            System.out.println("Not Found");
+        }
+        return -1;
+       
+    }
+
+}
