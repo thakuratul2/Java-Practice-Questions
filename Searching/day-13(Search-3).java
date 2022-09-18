dd81ed303d0e83701564607aea7e52a03595b350
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-18 23:34:59 +0530

    Day-13

--- Searching/day-13(Search-3).java
+++ Searching/day-13(Search-3).java
@@ -1,6 +1,26 @@
 import java.util.Scanner;
 
 class Search3{
+    public static int LinearAtul(int arr[], int n, int x) {
+        int first = -1;
+        int last = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                if (first == -1) {
+                    first = i;
+                }
+                last = i;
+            }
+        }
+        if (first != -1) {
+            System.out.println("First Occurrence: " + first);
+            System.out.println("Last Occurrence: " + last);
+        } else {
+            System.out.println("Not Found");
+        }
+        return -1;
+
+    }
 
     public static void main(String[] args) {
         //First and last occurrences of x
@@ -19,25 +39,6 @@ class Search3{
         LinearAtul(arr, size, key);
         sc.close();
     }
-    public static int LinearAtul(int arr[],int n,int x){
-        int first = -1;
-        int last = -1;
-        for(int i=0;i<n;i++){
-            if(arr[i]==x){
-                if(first==-1){
-                    first = i;
-                }
-                last = i;
-            }
-        }
-        if(first!=-1){
-            System.out.println("First Occurrence: "+first);
-            System.out.println("Last Occurrence: "+last);
-        }else{
-            System.out.println("Not Found");
-        }
-        return -1;
-       
-    }
+   
 
 }
