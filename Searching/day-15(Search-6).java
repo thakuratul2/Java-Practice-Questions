899f4b9660c10cabd8b41a5e008714b99a8f4cf2
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-20 23:15:38 +0530

    Day-15

---
+++ Searching/day-15(Search-6).java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Search6 {
+    public static String LeetCode1(String[] arr,String x){
+        int start = 0;
+        int last = arr.length-1;
+        int ans = 0;
+
+        int mid;
+        while(start<=last){
+            mid = (start+last)/2;
+            if(arr[mid]==x){
+                start = mid+1;
+            }
+            else{
+                ans = mid;
+                last = mid-1;
+            }
+        }
+        return arr[ans];
+    }
+    public static void main(String[] args) {
+        //find smallest number to greatest number than target
+        Scanner sc = new Scanner(System.in);
+        //int size = sc.nextInt();
+        System.out.println("Elements in arrays: ");
+        String arr[] = {"d", "j", "k"};
+        System.out.println(arr);
+
+        //loop 
+        
+        System.out.println("Enter the element to found: ");
+        String key = sc.nextLine();
+
+        String answer = LeetCode1(arr,key);
+        System.out.println("Found Element: "+answer);
+        sc.close();
+    }
+}
