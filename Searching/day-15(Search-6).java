6c8c86bb26ab63573202b3b247c47ce500c573ba
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-20 23:27:24 +0530

    Day-15

--- Searching/day-15(Search-6).java
+++ Searching/day-15(Search-6).java
@@ -6,7 +6,7 @@ class Search6 {
         int last = arr.length-1;
         int ans = 0;
 
-        int mid;
+        int mid=0;
         while(start<=last){
             mid = (start+last)/2;
             if(arr[mid]==x){
@@ -23,9 +23,9 @@ class Search6 {
         //find smallest number to greatest number than target
         Scanner sc = new Scanner(System.in);
         //int size = sc.nextInt();
-       
+        System.out.println("Elements in arrays: ");
         String arr[] = {"d", "j", "k"};
-
+        System.out.println(arr);
 
         //loop 
         
