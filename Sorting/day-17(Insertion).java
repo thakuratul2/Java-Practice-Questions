82bc752db309e7d13c550a5fb5f36a1c55fc482b
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-10-03 20:50:08 +0530

    Day-17

--- Sorting/day-17(Insertion).java
+++ Sorting/day-17(Insertion).java
@@ -1,7 +1,13 @@
 package Sorting;
 
+import java.util.Scanner;
+
 class Insertion {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+        System.out.println("Enter the element in array: ");
+        int[] arr = new int[size];
     }
 }
