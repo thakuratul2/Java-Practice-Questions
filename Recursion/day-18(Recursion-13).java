4b25e65e95d87628881e300cbd0835effe755758
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-10-05 20:16:21 +0530

    Day-18

--- Recursion/day-18(Recursion-13).java
+++ Recursion/day-18(Recursion-13).java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
 class Recursion13{
-    public static int Reverse(int num){
-        //reverse a digit by recursion
-        int sum = 0;
+    public static void Reverse(int num){
         if(num==0){
-            return num;
+            return;
         }
-        //return (num%10)*10 + Reverse(num/10);
-        int res = num%10;
-        sum = sum * 10 + res;
+        System.out.print(num%10);
         Reverse(num/10);
-        return sum;
-       // return sum;
+
+        
     }
     
    
@@ -21,8 +17,9 @@ class Recursion13{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int num = sc.nextInt();
-        int ans = Reverse(num);
-        System.out.println("Reverse of "+num+" is "+ans);
+        
+        System.out.print("Reverse Number is: ");
+        Reverse(num);
         sc.close();
 
        
