f344c28d230c7b24c5290f49945de042f88870a9
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-10-12 23:21:14 +0530

    Day-18

--- Recursion/day-18(Recursion-17).java
+++ Recursion/day-18(Recursion-17).java
@@ -1,10 +1,31 @@
 import java.util.Scanner;
 
 class Recursion17{
+    public static boolean LinearRec(int[] arr,int target,int index){
+        if(index==arr.length){
+            return false;
+        }
+        
+            return arr[index] == target || LinearRec(arr,target,index+1);
+    }
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+        System.out.println("Enter the element in array: ");
+        int[] arr= new int[size];
+
+        for(int i =0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the element: ");
+        int key = sc.nextInt();
+
+      boolean answer =  LinearRec(arr,key,0);
+      System.out.println("Found "+answer);
+
         sc.close();
     }
 }
