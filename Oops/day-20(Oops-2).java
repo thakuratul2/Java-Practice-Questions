54cd3cd56a052f6a1e8f696217dde74c11ceb7ab
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-10-22 21:00:39 +0530

    Day-20

--- Oops/day-20(Oops-2).java
+++ Oops/day-20(Oops-2).java
@@ -1,7 +1,10 @@
 package Oops;
 
+import java.util.Scanner;
+
 class Oops2{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        sc.close();
     }
 }
