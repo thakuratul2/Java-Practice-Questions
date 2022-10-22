79651f86bd5264f7357333d02f4f010174b100f5
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-10-22 21:04:47 +0530

    Day-20

--- Oops/day-20(Oops-1).java
+++ Oops/day-20(Oops-1).java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 class Oops1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
         sc.close();
     }
 }
